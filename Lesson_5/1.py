e4f4c407bcfbec67b8f95c822ac306a2a57ece1a
Kracavchik <sir.pasha1512@yandex.ru>
2020-01-24 07:47:05 +0300

    Homework done

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,44 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+
+from collections import namedtuple
+
+
+def company_profit():
+    company_count = int(input("Введите число компаний: "))
+    companies = namedtuple(
+        'Company',
+        'name quarter_1 quarter_2 quarter_3 quarter_4')
+    profit_per_company = {}
+    for i in range(company_count):
+        company = companies(
+            name=input(f"Введите имя {i+1}-й компании: "),
+            quarter_1=int(
+                input('Введите прибыль за первый квартал: ')),
+            quarter_2=int(
+                input('Введите прибыль за второй квартал: ')),
+            quarter_3=int(
+                input('Введите прибыль за третий квартал: ')),
+            quarter_4=int(
+                input('Введите прибыль за четвертый квартал: ')))
+        profit_per_company[company.name] = (
+            company.quarter_1 + company.quarter_2 + company.quarter_3 + company.quarter_4) / 4
+    total_profit = 0
+    for value in profit_per_company.values():
+        total_profit += value
+    total_profit = total_profit / company_count
+
+    for i, j in profit_per_company.items():
+        if j < total_profit:
+            print(f"Прибыль компании - {i}, ниже средней.")
+    for i, j in profit_per_company.items():
+        if j > total_profit:
+            print(f"Прибыль компании - {i}, выше средней.")
+
+
+company_profit()
+
+# Каюсь, код подсмотрел на 6-м уроке, потому что не до конца понял как работать
+# с новыми коллекциями, но сейчас наконец то подразобрался.
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,25 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+
+import collections
+
+
+def hex_val(num):
+    hex_v = 0
+    for i in range(len(num)):
+        hex_v += int(num[i], 16) * (16 ** (len(num) - i - 1))
+    return hex_v
+
+
+hex_numbers = collections.defaultdict(list)
+hex_numbers[1] = list(input("Введите первое число: "))
+hex_numbers[2] = list(input("Введите второе число: "))
+
+hex_sum = str(hex(hex_val(hex_numbers[1]) + hex_val(hex_numbers[2])))[2:]
+hex_multiple = str(hex(hex_val(hex_numbers[1]) * hex_val(hex_numbers[2])))[2:]
+hex_numbers[hex_sum] = list(hex_sum)
+hex_numbers[hex_multiple] = list(hex_multiple)
+
+print(f"Сумма чисел {hex_numbers[hex_sum]}, Произведение {hex_numbers[hex_multiple]}")
