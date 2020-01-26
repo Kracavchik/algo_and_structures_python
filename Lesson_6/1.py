e6d0e79807afca560983e5de4040acf43131182f
Kracavchik <sir.pasha1512@yandex.ru>
2020-01-26 07:59:43 +0300

    Homework done

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,98 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+
+import sys
+import random
+import time
+import memory_profiler
+from memory_profiler import profile
+
+
+# Lesson_1_4
+@profile
+def first_function():
+    t_p = time.process_time()
+    m_p = memory_profiler.memory_usage()
+    int_start = 1
+    int_stop = 1000000
+    print(f'Случайное число: {random.randint(int_start, int_stop)}')
+
+    float_start = 1
+    float_stop = 1000000
+    print(
+        f'Случайное вещественное число: {random.random() * (float_stop - float_start) + float_start}')
+
+    char_start = 'a'
+    char_stop = 'b'
+    print(
+        f'Случаный символ: {chr(random.randint(ord(char_start), ord(char_stop)))}')
+
+    print(f'Выполнение заняло {t_p} сек и {m_p[0]} Мб')
+
+
+'''
+Windows 10 x64 Python 3.8
+Время исполнения скрипта(не считая profile) ~0.1 сек, занимаемое количество памяти менее 0.0 Мб.
+'''
+
+
+# Lesson_2_3
+def second_function():
+    t_p = time.process_time()
+    m_p = memory_profiler.memory_usage()
+
+    @profile
+    def recursion(number: int):
+        """Принимает число и возвращает его цифры в обратном порядке"""
+        answer = ''
+        if len(str(number)) == 1:
+            answer += str(number)
+            return answer
+        answer = (answer + str(number % 10)) + str(recursion(number // 10))
+        return answer
+
+    while True:
+        a = random.randint(1, 100)
+        try:
+            a = int(a)
+            print(recursion(a))
+            print(f'Выполнение заняло {t_p} сек и {m_p[0]} Мб')
+            sys.exit(1)
+        except ValueError:
+            print('Это не число!')
+
+
+'''
+Windows 10 x64 Python 3.8
+Время исполнения скрипта(не считая profile) ~0.1 сек, занимаемое количество памяти менее 0.0 Мб.
+'''
+
+
+# Lesson_3_8
+@profile
+def third_function():
+    t_p = time.process_time()
+    m_p = memory_profiler.memory_usage()
+    matrix = []
+    for i in range(4):
+        line = []
+        a = 4
+        for j in range(4):
+            line.append(int(random.randint(0, 10)))
+            a -= 1
+        line.append(sum(line))
+        matrix.append(line)
+    for i in matrix:
+        print(f'{i}')
+    print(f'Выполнение заняло {t_p} сек и {m_p[0]} Мб')
+
+
+'''
+Windows 10 x64 Python 3.8
+Время исполнения скрипта(не считая profile) ~0.1 сек, занимаемое количество памяти менее 0.0 Мб.
+'''
+
+# first_function()
+# second_function()
+third_function()
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,32 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+
+
+from pympler import asizeof
+
+
+class Human:
+    """Тестовый класс 1"""
+    __slots__ = ('first_name', 'last_name')
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class NewHuman:
+    """Тестовый класс 2"""
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+ALEXANDR = Human('Alexandr', 'Nevsky')
+print(asizeof.asizeof(ALEXANDR))
+VLADIMIR = NewHuman('Vladimir', 'Johnson')
+print(asizeof.asizeof(VLADIMIR))
+print(VLADIMIR.__dict__)
+
+# Класс с применением слотов даёт более 50% экономию памяти,
+# при невозможности добавления доп атрибутов.
